60915d299e4aaf69e3b75f4f97548b8fd2008303
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-15 13:32:20 +0900

    [Add] App Header

---
+++ components/00_globals/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row justify="center" align-content="center">
+      <v-col cols="1">
+        <app-logo />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AppLogo from './AppLogo'
+export default {
+  name: 'AppHeader',
+  components: { AppLogo }
+}
+</script>
