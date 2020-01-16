90777c5f6b8d7fca729c4cf2db55c4957fb4fb32
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-17 01:02:20 +0900

    [Fix] env variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,5 +57,10 @@ export default {
       })
       return routing
     }
+  },
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_ACCESS_TOKEN: process.env.CTF_ACCESS_TOKEN,
+    CTF_CONTENT_TYPE_ID: process.env.CTF_CONTENT_TYPE_ID
   }
 }
