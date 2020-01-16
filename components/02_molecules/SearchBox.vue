2cbcf4a3e2ef32a0af93ac0b65d813c66a2b7a72
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-17 02:42:09 +0900

    [Add] Search Box

---
+++ components/02_molecules/SearchBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row justify="center" align-content="center">
+      <v-col cols="6">
+        <v-text-field
+          v-model="inputKeyword"
+          placeholder="記事を検索"
+          outlined
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  data() {
+    return {
+      keyword: ''
+    }
+  },
+  computed: {
+    inputKeyword: {
+      get() {
+        return this.keyword
+      },
+      set(keyword) {
+        this.keyword = keyword
+        this.$emit('keyword', keyword)
+      }
+    }
+  }
+}
+</script>
