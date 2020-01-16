1d165a29abd03e7ed778ef4d75b700f89f643f9c
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-16 18:43:10 +0900

    [Add] link to card

--- components/02_molecules/CardItem.vue
+++ components/02_molecules/CardItem.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-card width="300" height="225" max-width="300" max-height="225">
-    <card-img :src="src" />
-    <card-title :title="title" />
-    <card-text :text="text" />
-  </v-card>
+  <nuxt-link :to="url" style="text-decoration: none">
+    <v-card width="300" height="225" max-width="300" max-height="225">
+      <card-img :src="src" />
+      <card-title :title="title" />
+      <card-text :text="text" />
+    </v-card>
+  </nuxt-link>
 </template>
 
 <script>
@@ -14,6 +16,10 @@ export default {
   name: 'CardItem',
   components: { CardText, CardTitle, CardImg },
   props: {
+    slug: {
+      type: String,
+      default: ''
+    },
     src: {
       type: String,
       default: ''
@@ -25,6 +31,11 @@ export default {
     text: {
       type: String,
       default: ''
+    }
+  },
+  computed: {
+    url() {
+      return `/posts/${this.slug}`
     }
   }
 }
