ce27a6e8d659e122510befb8eb9b8089914a2363
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-16 18:35:10 +0900

    [Add] card to top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-container>
-    <p>Init</p>
-  </v-container>
+  <div>
+    <card-item-list :cards="entries" />
+  </div>
 </template>
+<script>
+import CardItemList from '../components/03_organisms/CardItemList'
+import { findAllEntries } from '../plugins/contentful'
+export default {
+  components: { CardItemList },
+  async asyncData() {
+    const entries = await findAllEntries()
+    return { entries }
+  }
+}
+</script>
