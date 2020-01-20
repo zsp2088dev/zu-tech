95ec4e8b97fc43002c173bf68eb45080045edc1d
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-21 04:04:26 +0900

    [Add] Post Components

---
+++ components/01_atoms/PostImg.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-img :alt="title" :src="src" width="640px" height="275px" />
+</template>
+
+<script>
+export default {
+  name: 'PostImg',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    src: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
