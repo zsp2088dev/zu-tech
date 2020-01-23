5bdda6857b18d2e8ece178c0f22cdb6d09e390d7
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-23 16:55:54 +0900

    [Fix] env variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 export default {
   mode: 'spa',
   head: {
-    title: website.title,
+    title: process.env.WEBSITE_TITLE,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -68,6 +68,7 @@ export default {
     }
   },
   env: {
+    WEBSITE_TITLE: website.title,
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_ACCESS_TOKEN: process.env.CTF_ACCESS_TOKEN,
     CTF_CONTENT_TYPE_ID: process.env.CTF_CONTENT_TYPE_ID
