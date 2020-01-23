76d8becbcc7d37e0b3e0fc72820d583228042081
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-23 14:41:20 +0900

    [Update] Share Button

---
+++ components/02_molecules/ShareItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <a :href="shareUrl" target="blank">
+    <share-btn :service="service" />
+  </a>
+</template>
+
+<script>
+import ShareBtn from '../01_atoms/ShareBtn'
+import { generateShareUrl } from '../../plugins/sns'
+export default {
+  name: 'ShareItem',
+  components: { ShareBtn },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    service: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    shareUrl() {
+      const url = `https://zu-tech.netlify.com${this.$route.path}`
+      return generateShareUrl(this.service, this.title, url)
+    }
+  }
+}
+</script>
