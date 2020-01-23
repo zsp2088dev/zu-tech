e63398eaa53cf010b456d44e68975509fcf05599
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-23 20:48:42 +0900

    [Fix] image url

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: '~/image.png'
+        content: '/image.png'
       },
       {
         name: 'twitter:site',
@@ -59,7 +59,7 @@ export default {
       },
       {
         name: 'twitter:image',
-        content: '~/image.png'
+        content: '/image.png'
       },
       {
         name: 'twitter:description',
