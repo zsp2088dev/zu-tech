8d545d76dfd43de2470afd48a9966aa32be8c629
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-24 02:23:55 +0900

    [Change] base ogp image

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: '/image.png'
+        content: `${website.url}/image.png`
       },
       {
         hid: 'twitter:card',
