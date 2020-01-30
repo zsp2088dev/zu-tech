88e3fa86ac74975dcb7cbcb9c849dc3176319108
Kazuki Yamagata <zsp2088dev@gmail.com>
2020-01-30 14:58:03 +0900

    [Add] AppFooter

---
+++ components/00_globals/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer absolute>
+    <v-col cols="12" class="text-center">
+      <small class="copyright">© 2020 ZU TECH.</small>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style scoped lang="scss">
+.copyright {
+  color: $primary-text;
+  font-weight: 300;
+}
+</style>
